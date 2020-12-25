613b83f189734233319afbc3c70ee1dee039f37c
edimdendidiwangga <edimdendy@gmail.com>
2020-12-26 06:18:30 +0700

    Working with Constraints

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
     return Object.assign(objA, objB);
 }
 
 const mergeObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
-console.log(mergeObj.age);
+console.log(mergeObj);
